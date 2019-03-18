236da97dddb3de2f2f1c56f3065ebb890120df18
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-03-18 11:11:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,21 +1,21 @@
 # your code goes here
 
 class Person
-  # attr_accessor :balance
+  attr_accessor :balance
   attr_reader :name
   
-  def initialize(name)
+  def initialize(name, balance)
     @name = name
-    # @balance = balance
+    @balance = balance
   end
   
   def name
     @name
   end
   
-  # def self.bank_account(balance= 25)
-  #   @balance
-  # end
+  def bank_account(balance= 25)
+    @balance
+  end
   
   
 end
